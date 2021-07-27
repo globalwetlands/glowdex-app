71a8561333d26a6278c83be701f2baaf2ad27ce1
Eric Jinks <ericjinks@gmail.com>
2021-07-28 09:13:47 +1000

    Horizontal box plot

--- src/features/widgets/charts/TypologyBoxPlot.js
+++ src/features/widgets/charts/TypologyBoxPlot.js
@@ -20,21 +20,28 @@ const config = {
 const layout = {
   plot_bgcolor: 'transparent',
   paper_bgcolor: 'transparent',
-  showlegend: true,
+  showlegend: false,
+  legend: {
+    x: 0,
+    y: 2,
+  },
   hovermode: false,
   margin: {
-    l: 15,
+    l: 200,
     r: 0,
-    t: 0,
-    b: 100,
-  },
-  yaxis: {
-    // title: 'Val',
-    zeroline: false,
-    range: [-5, 5],
+    t: 15,
+    b: 15,
   },
   xaxis: {
-    title: 'Indicator',
+    // title: 'Val',
+    showticklabels: 'true',
+    zeroline: true,
+    range: [-4, 4.1],
+    mirror: 'ticks',
+    tickmode: 'linear',
+  },
+  yaxis: {
+    // title: 'Indicator',
     // tickangle: -90,
     showticklabels: true,
     tickfont: {
@@ -42,7 +49,6 @@ const layout = {
     },
   },
   boxgap: 0.1,
-  height: 400,
 }
 
 function getSignificanceFactorColor(significanceFactor) {
@@ -115,11 +121,11 @@ export function TypologyBoxPlot({ gridItems, gridItem, quantileValue = 0.8 }) {
     ({ colName, significanceFactor }) => {
       const color = getSignificanceFactorColor(significanceFactor)
       // Accessing residuals data
-      const y = gridItems.map((item) => item.residuals[colName])
+      const x = gridItems.map((item) => item.residuals[colName])
       const displayName = indicatorColnames[colName]
       const boxplot = {
         type: 'box',
-        y,
+        x,
         name: displayName,
         showlegend: false,
         // whiskerwidth: 0.2,
@@ -137,8 +143,8 @@ export function TypologyBoxPlot({ gridItems, gridItem, quantileValue = 0.8 }) {
     ({ colName }) => {
       const displayName = indicatorColnames[colName]
       const name = `Cell ${displayName}`
-      const x = [displayName]
-      const y = [gridItem.residuals[colName]]
+      const y = [displayName]
+      const x = [gridItem.residuals[colName]]
       const marker = {
         x,
         y,
@@ -181,11 +187,14 @@ export function TypologyBoxPlot({ gridItems, gridItem, quantileValue = 0.8 }) {
   ]
 
   const calculatedLayout = useMemo(() => {
+    const rowHeight = 40
+    const height = significantIndicatorColumns.length * rowHeight
     return {
       ...layout,
       width,
+      height,
     }
-  }, [width])
+  }, [significantIndicatorColumns.length, width])
 
   return (
     <div ref={containerRef} style={{ width: '100%' }}>
